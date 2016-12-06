44943dc0ce4661dd797487135599af989b63ac16
CODE-AN <ayumu.nakamura@std.code.ac.jp>
2016-12-06 09:21:20 +0900

    Project stage01 animation and coding completion

--- js/bootlog.js
+++ js/bootlog.js
@@ -1,5 +1,5 @@
 // bootlog.js
-var bootmsg = ["CODEACADEMY System Ver.1.0.0 rev.1000<br>",">>Initalize System ...","OK<br>",">>Detecting boot device ... ","OK<br>",">>Loading kernel ... ","OK<br>",">>Detecting OS control device ... ","OK<br>",">>Booting...<br>",">>Processor 0 status ... ","Active<br>",">>Processor 1 status ... ","Active<br>",">>Processor 2 status ... ","Active<br>",">>Processor 3 status ... ","Active<br>",">>Processor 4 status ... ","Disable[HT]<br>",">>Processor 5 status ... ","Disable[HT]<br>",">>Processor 6 status ... ","Disable[HT]<br>",">>Processor 7 status ... ","Disable[HT]<br>",">>Memory Initialize 32767/32767MBytes<br>","<br>","INIT: Kernal version 1.00 booting...<br>","<br>","ROSS:<br>","<br>","Mounting proc at /proc...          ","[OK]<br>","Mounting sysfs at /sts...          ","[OK]<br>","Initialize network service<br>","Setting up localhost...            ","[OK]<br>","Setting up ethernet...             ","[OK]<br>","Setting up access route...         ","[OK]<br>","Accessing authentication server... ","[OK]<br>","Stating system log at /log/sys...  ","[OK]<br>","Cleanning /var/lock                ","[OK]<br>","Cleaning /tmp                      ","[OK]<br>","Updating init.rc                   ","[OK]<br>",">>codeac-server.sys : UX Graphics Module Loading..."];
+var bootmsg = ["CODE ACADEMY System Ver.1.0.0 rev.1000<br>",">>Initalize System ...","OK<br>",">>Detecting boot device ... ","OK<br>",">>Loading kernel ... ","OK<br>",">>Detecting OS control device ... ","OK<br>",">>Booting...<br>",">>Processor 0 status ... ","Enable<br>",">>Processor 1 status ... ","Enable<br>",">>Processor 2 status ... ","Enable<br>",">>Processor 3 status ... ","Enable<br>",">>Processor 4 status ... ","Enable[HT]<br>",">>Processor 5 status ... ","Enable[HT]<br>",">>Processor 6 status ... ","Enable[HT]<br>",">>Processor 7 status ... ","Enable[HT]<br>",">>Memory Initialize 32767/32767MBytes<br>","<br>","INIT: Kernel version 1.00 booting...<br>","<br>","ROSS:<br>","<br>","Mounting proc at /proc...          ","[OK]<br>","Mounting sysfs at /sts...          ","[OK]<br>","Initialize network service<br>","Setting up localhost...            ","[OK]<br>","Setting up ethernet...             ","[OK]<br>","Setting up access route...         ","[OK]<br>","Accessing authentication server... ","[OK]<br>","Stating system log at /log/sys...  ","[OK]<br>","Cleanning /var/lock                ","[OK]<br>","Cleaning /tmp                      ","[OK]<br>","Updating init.rc                   ","[OK]<br>",">>codeac-server.sys : UX Graphics Module Loading..."];
 
 function bootlogtxt() {
   var bootmsgl = bootmsg.length;
@@ -10,7 +10,7 @@ function bootlogtxt() {
 	txt += bootmsg[count];
 	target = document.getElementById("bootlog");
     target.innerHTML = txt;
-    console.log(count++);
+    count++;
     var id = setTimeout(countup, timer);
 	if(count == 1){timer = 200;}
 	if(count == 9){timer = 2000;}
